e74842f50b868f4131bc026fb572db81424a15dd
Mark T <aydenthos@gmail.com>
2023-09-18 14:53:22 -0700

    lots of updates cleaning up code, testing basic middleware and socket function.  DB is fully operational now.  Next will work on models and final seeding

---
+++ controllers/api/userRoutes.js
@@ -0,0 +1,34 @@
+const express = require("express");
+
+// Adjust the path to your User model
+const User = require("../../models").User;
+
+const router = express.Router();
+
+// get all users
+router.get("/", async (req, res) => {
+  try {
+    const userData = await User.findAll();
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// get user by id
+router.get("/:id", async (req, res) => {
+  try {
+    const userData = await User.findByPk(req.params.id);
+    if (!userData) {
+      res.status(404).json({ message: "No user with this id!" });
+      return;
+    }
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -5,16 +5,14 @@ const exphbs = require("express-handlebars");
 const routes = require("./controllers");
 const helpers = require("./utils/helpers");
 
-// Hi Will, I added this to the top of the file
-
 const sequelize = require("./config/connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Set up Handlebars.js engine with custom helpers
-const hbs = exphbs.create({ helpers });
+// // Set up Handlebars.js engine with custom helpers
+// const hbs = exphbs.create({ helpers });
 
 const sess = {
   secret: process.env.SECRET,
@@ -34,19 +32,41 @@ const sess = {
 
 app.use(session(sess));
 
-// Inform Express.js on which template engine to use
-app.engine("handlebars", hbs.engine);
-app.set("view engine", "handlebars");
+// // Inform Express.js on which template engine to use
+// app.engine("handlebars", hbs.engine);
+// app.set("view engine", "handlebars");
 
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
-// Use the login-related middleware
-app.use(require("./controllers/login"));
+
+
+// Setup Socket.io
+const http = require("http");
+const server = http.createServer(app);
+const io = require("socket.io")(server);
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+  
+  socket.on("error", (error) => {
+    console.error("Socket error:", error);
+  });
+});
+
+app.use((req, res, next) => {
+  req.io = io; // Attach the io instance to the req object
+  next();
+});
 
 app.use(routes);
+
 
 // sync sequelize models to the database, then turn on the server
 sequelize.sync({ force: false }).then(() => {
