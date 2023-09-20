91f03069f4e9e60f9a51fea8414b95ef81769742
Mark T <aydenthos@gmail.com>
2023-09-19 22:05:10 -0700

    Berkely styling, adjusting some wording, building dashboard.handlebars, added ordering to the blogpost routing, added dashboard get routing

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -50,9 +50,39 @@ router.get("/homepage", async (req, res) => {
             attributes: ["first_name", "last_name"],
           },
         ],
+        order: [["date_created", "DESC"]],
       });
       const blogs = blogData.map((blog) => blog.get({ plain: true }));
       res.render("homepage", {
+        blogs,
+        user_name: req.session.user_name,
+        logged_in: req.session.logged_in,
+        last_logged: req.session.last_logged,
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(500).send("Fly you fools. Server Error");
+    }
+  });
+
+// Takes you to the dashboard.handlebars which renders all the blog posts that the user has made
+
+router.get("/dashboard", async (req, res) => {
+    try {
+      const blogData = await BlogPost.findAll({
+        where: {
+          user_id: req.session.user_id,
+        },
+        include: [
+          {
+            model: User,
+            attributes: ["first_name", "last_name"],
+          },
+        ],
+        order: [["date_created", "DESC"]],
+      });
+      const blogs = blogData.map((blog) => blog.get({ plain: true }));
+      res.render("dashboard", {
         blogs,
         user_name: req.session.user_name,
         logged_in: req.session.logged_in,
